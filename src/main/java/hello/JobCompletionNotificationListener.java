bc9ba375e2cc469abb9bf4c4a3cf430901dcd19d
Nena Park <nena.park@gmail.com>
2019-04-09 22:28:02 -0400

    solved job completion notifier errors. Need to solve batch configuration reader error - student details

--- src/main/java/hello/JobCompletionNotificationListener.java
+++ src/main/java/hello/JobCompletionNotificationListener.java
@@ -1,5 +1,36 @@
 package hello;
 
-public class JobCompletionNotificationListener {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
-}
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+	private final JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+			log.info("!!! JOB FINISHED! Time to verify the results");
+
+			jdbcTemplate.query("",
+				(rs, row) -> new Student(rs.getString(1),
+						rs.getLong(2), rs.getString(3), rs.getString(4), rs.getString(5))
+				).forEach(student-> log.info("Found <" + student + "> in the database."));
+			}
+		}
+	}
+
